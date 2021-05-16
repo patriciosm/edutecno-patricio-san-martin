2a5bb6f6eb6f6c9c9fdd2f85b1d798418c162aa1
Patricio San Martin <patriciosm@gmail.com>
2021-05-16 02:09:04 -0400

    Se corrigen y agregan estilos, agrega tooltips

--- viajes_chile/assets/js/script.js
+++ viajes_chile/assets/js/script.js
@@ -6,4 +6,14 @@ $(function () {
     .mouseleave(function(){
         $(this).find(".card-body").css({"background-color":"inherit","color":"#fff"});
     });
+
+    $(".navbar-nav .nav-item")
+    .mouseenter(function(){
+        $(this).addClass("item-hover");
+    })
+    .mouseleave(function(){
+        $(this).removeClass("item-hover");
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
 })
